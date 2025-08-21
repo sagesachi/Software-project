84a610add11322baa64576359c5323a5764a0e86
sagesachi <emily.stening@education.nsw.gov.au>
2025-08-21 11:13:21 +1000

    Update text-based-adventure-game.py

--- text-based-adventure-game.py
+++ text-based-adventure-game.py
@@ -4,15 +4,22 @@ import renpy
 def intro():
   print("you decide your own fate")
   print("type the corrosponding number to make a choice.")
+  time.sleep()
+  print("while walking one afternoon, you stumble upon a new area in which you come to a crossroads")
+  time.sleep()
+  print("You have the choice to go in one of three directions.")
+  print("The direction of an old house on a nearby hill(1)
+  print("A misty forest(2)")
+  print("Or a nearby silent graveyard(3)...")
 
 def choosepath():
   path == ""
-  while path != = "1" and path != "2" and path != "3":
+  while path != = "1" or path != "2" or path != "3":
     path = input("Which path will you choose? (1, 2 or 3): ")
   return path
 
 def checkpath(path):
-  print("you make your way towards your destination...")
+  print("You make your way towards your destination...")
   time.sleep()
 
   if path == "1":
@@ -21,7 +28,8 @@ def checkpath(path):
   elif path == "2":
     pass
     
-  elif path == "3":
+  else path == "3":
     pass
-  
+
+
   
