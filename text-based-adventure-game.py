eeb213f3b673f1e5cae011da14ad4498b3cfe87c
sagesachi <emily.stening@education.nsw.gov.au>
2025-08-21 11:35:09 +1000

    Update text-based-adventure-game.py

--- text-based-adventure-game.py
+++ text-based-adventure-game.py
@@ -1,5 +1,21 @@
 import time
+import pygame
+from pygame.locals import *
+pygame.init()
 
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+class Character1(pygame.sprite.Sprite):
+  def __init__(self):
+    self.image = pygame.image.load("Character1.png")
+    self.rect = self.image.get_rect()
+
+class Character2(pygame.sprite.Sprite):
+  def __init__(self):
+    self.image = pygame.image.load("Character2.png")
+    self.rect = self.image.get_rect()
+    
 def intro():
   print("you decide your own fate")
   print("type the corrosponding number to make a choice.")
