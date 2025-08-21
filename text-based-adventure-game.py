9260c68231d1d53a0d29e38a740831db72c3b6a4
sagesachi <emily.stening@education.nsw.gov.au>
2025-08-21 10:57:47 +1000

    Create text-based-adventure-game.py

---
+++ text-based-adventure-game.py
@@ -0,0 +1,16 @@
+import time
+import renpy
+import random
+
+def intro():
+  print("you decide your own fate")
+  print("type the corrosponding number to make a choice.")
+
+def choosepath():
+  path == ""
+  while path != = "1" and path != "2" and path != "3":
+    path = input("Which path will you choose? (1, 2 or 3): ")
+  return path
+
+def checkpath(choosepath):
+  pass
