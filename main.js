5b6f223c6f2cb7034509823f3f0774ea1db178e6
sagesachi <emily.stening@education.nsw.gov.au>
2025-08-25 09:28:34 +1000

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function getStory(name)  {
     currentScene: "path",
     path: {
       title: "Choose your path",
-      story: `One day during a normal walk in the evening, you come across a intersection of 3 different pathways. Each of the pathways lead to a different destination, an old house on a nearby hill(1), a silent graveyard(2) and a misty forest(3).',
+      story: `One day during a normal walk in the evening, you come across a intersection of 3 different pathways. Each of the pathways lead to a different destination, an old house on a nearby hill(1), a silent graveyard(2) and a misty forest(3).`,
       choices: [
         {
           choice: "Up the hill towards the old house",
