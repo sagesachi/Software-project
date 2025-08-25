aa2a8741e87b9a7a62540fab35b5729cca62003c
sagesachi <emily.stening@education.nsw.gov.au>
2025-08-25 10:06:32 +1000

    Update main.js

--- main.js
+++ main.js
@@ -114,3 +114,56 @@ document.addEventListener('DOMContentLoaded', function() {
     renderScene()
   })
 })
+
+function renderScene() {
+  var text = "Next"
+  var image = "";
+  if (story[story.currentScene].image) {
+    image = "<img></img>"
+  }
+  if (story[story.currentScene].buttonText) {
+    text = story[story.currentScene].buttonText
+  }
+  content.innerHTML = `
+  <h1>${story[story.currentScene].title}</h1>
+  <p>${story[story.currentScene].story}</p>
+  ${image}
+  ${getInputs()}
+  <button id = "submit-button">${text}</button>
+  `
+  if (story[story.currentScene].image) {
+    document.querySelector("img").src = `./img/${story[story.currentScene].image}`
+  }
+  var button = document.querySelector("#submit-button");
+  button.addEventListener('click', function() {
+    getInputValue()
+  })
+}
+
+function getInputValue() {
+  var inputs = document.querySelectorAll('input[type="radio"]');
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].checked) {
+      story.currentScene = inputs[i].getAttribute('data-destination')
+      renderScene();
+      return;
+    }
+  }
+  story.currentScene = story[story.currentScene].defaultDestination
+  renderScene()
+}
+
+function getInputs() {
+  var input = ""
+  if (!story[story.currentScene].choices) {
+    return ""
+  }
+  for(var i = 0; i < story[story.currentScene].choices.length; i++) {
+    input += `
+    <div>
+      <input data-destination = ${story[story.currentScene].choices[i].destination} id = "radio${i}" type = "radio" name = "choices" />
+      <label for "radio${i}">${story[story.currentScene].choices[i].choice}</label>
+    </div>`
+  }
+  return input;
+}
