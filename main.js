7ef13c5f9471b129e03575d9e0f03caa52fe687f
sagesachi <emily.stening@education.nsw.gov.au>
2025-08-26 12:08:56 +1000

    Update main.js

--- main.js
+++ main.js
@@ -111,53 +111,53 @@ function getStory(name)  {
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  var button = document.querySelector('#start-button')
-  var content = document.querySelector('#content')
-  button.addEventListener('click', function() {
-    var name = document.querySelector('#name-input')
-    story = getStory(name.value)
-    renderScene()
-  })
-})
+//document.addEventListener('DOMContentLoaded', function() {
+//  var button = document.querySelector('#start-button')
+//  var content = document.querySelector('#content')
+//  button.addEventListener('click', function() {
+//    var name = document.querySelector('#name-input')
+//    story = getStory(name.value)
+ //   renderScene()
+//  })
+//})
 
-function renderScene() {
-  var text = "Next"
-  var image = "";
-  if (story[story.currentScene].image) {
-    image = "<img></img>"
-  }
-  if (story[story.currentScene].buttonText) {
-    text = story[story.currentScene].buttonText
-  }
-  content.innerHTML = `
-  <h1>${story[story.currentScene].title}</h1>
-  <p>${story[story.currentScene].story}</p>
-  ${image}
-  ${getInputs()}
-  <button id = "submit-button">${text}</button>
-  `
-  if (story[story.currentScene].image) {
-    document.querySelector("img").src = `./img/${story[story.currentScene].image}`
-  }
-  var button = document.querySelector("#submit-button");
-  button.addEventListener('click', function() {
-    getInputValue()
-  })
-}
+//function renderScene() {
+//  var text = "Next"
+//  var image = ""
+//  if (story[story.currentScene].image) {
+//    image = "<img></img>"
+//  }
+//  if (story[story.currentScene].buttonText) {
+//    text = story[story.currentScene].buttonText
+//  }
+//  content.innerHTML = `
+//  <h1>${story[story.currentScene].title}</h1>
+//  <p>${story[story.currentScene].story}</p>
+//  ${image}
+//  ${getInputs()}
+//  <button id = "submit-button">${text}</button>
+//  `
+//  if (story[story.currentScene].image) {
+//    document.querySelector("img").src = `./img/${story[story.currentScene].image}`
+//  }
+//  var button = document.querySelector("#submit-button");
+//  button.addEventListener('click', function() {
+//    getInputValue()
+//  })
+//}
 
-function getInputValue() {
-  var inputs = document.querySelectorAll('input[type="radio"]');
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].checked) {
-      story.currentScene = inputs[i].getAttribute('data-destination')
-      renderScene();
-      return;
-    }
-  }
-  story.currentScene = story[story.currentScene].defaultDestination
-  renderScene()
-}
+//function getInputValue() {
+//  var inputs = document.querySelectorAll('input[type="radio"]');
+//  for (var i = 0; i < inputs.length; i++) {
+//    if (inputs[i].checked) {
+//      story.currentScene = inputs[i].getAttribute('data-destination')
+//      renderScene();
+//      return;
+//    }
+//  }
+//  story.currentScene = story[story.currentScene].defaultDestination
+//  renderScene()
+//}
 
 function getInputs() {
   var input = ""
